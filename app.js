484f52f70a7b7776e3f97b98970f3ac079bbb4ba
vikashpathak01 <140196460+vikashpathak01@users.noreply.github.com>
2024-10-25 07:42:43 +0530

    Update app.js
    churki furki added

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ window.onload = () => {
 function calculateDecimal() {
     const katha = parseFloat(document.querySelector('#katha').value);
     const dhur = parseFloat(document.querySelector('#dhur').value);
+    const furki = parseFloat(document.querySelector('#furki').value);
+    const churki = parseFloat(document.querySelector('#churki').value);
     const result = document.querySelector('#result');
 
   
    
-
+ 
     // BMI Calculation
-    const bmi = (((katha * 20) + dhur) / 5.375 ).toFixed(4);
+    const bmi =   (((((((((katha * 20) + dhur) * 20 ) + dhurki ) * 20) + furki) * 20 ) + churki ) / 5.375 ).toFixed(3);
+    // const bmi = (((katha * 20) + dhur) / 5.375 ).toFixed(4);
     result.innerText = `Total Decimal: ${bmi}`; 
 
    
